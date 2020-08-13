60f0fbce4bdf8f58703ee4472de487569ffb2429
bhavanakota <46168256+bhavanakota@users.noreply.github.com>
2020-08-13 14:48:04 -0500

    Update Exercise1.java

--- Lab 6/src/Exercise1.java
+++ Lab 6/src/Exercise1.java
@@ -1,3 +1,4 @@
+
 /* Write the simplest Java method that would have 𝑂(𝑁3) time complexity,
  * where 𝑁 is the single input parameter to the method.
  */
@@ -8,16 +9,23 @@ public class Exercise1 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 
+		// Enter a value for n that's inputted into method complexity 
 		System.out.print("Enter a value for n: ");
 		int n = scan.nextInt();
+		complexity(n);
+		
+		scan.close();
+	}
 
+	public static void complexity(int n) {
+		// Creating the triple nested loops to form O(n^3)
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= n; j++) {
 				for (int k = 1; k <= n; k++) {
+					// Prints out the values for O(n), O(n^2), and O(n^3)
 					System.out.println("O(n) = " + i + " O(n^2) = " + j + " O(n^3) = " + k);
 				}
 			}
 		}
-		scan.close();
 	}
 }
