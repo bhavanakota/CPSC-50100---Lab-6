ea9ee75c329223d389c214558fb71f94d6c0c437
bhavanakota <46168256+bhavanakota@users.noreply.github.com>
2020-08-13 14:58:25 -0500

    Update Exercise2.java

--- Lab 6/src/Exercise2.java
+++ Lab 6/src/Exercise2.java
@@ -15,7 +15,7 @@ public class Exercise2 {
 
 	/*
 	 * The first loop is size N will be O(N) and the time complexity of the second
-	 * loop is of size 1000000 so then the complexity will be only O(N + 1000000) since it's
+	 * loop is of size 1000000 so then the complexity will be only O(N) since it's
 	 * only dependent on the first loop since N is greater than 1000000.
 	 */
 
